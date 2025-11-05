4824a5b9c89541c548cf27da6843eb159325465b
Javier-Coronel <90841478+Javier-Coronel@users.noreply.github.com>
2025-11-05 14:56:47 +0100

    Started activities implementation

---
+++ alta_actividad.php
@@ -0,0 +1,16 @@
+<?php
+include_once("cabecera.html");
+?>
+<div class="container" id="alta">
+    <div class="row">
+        <form class="form-horizontal" action="proceso_alta_componente.php" name="frmAltaActividad" id="frmAltaActividad" method="post">
+            <fieldset>
+                <legend>Alta de actividad</legend>
+            </fieldset>
+        </form>
+    </div>
+
+
+</div>
+</body>
+</html>
